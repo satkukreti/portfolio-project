45ccd6b563925536e211348b20b5b60d41906b46
unknown <kdatt1@yahoo.com>
2020-05-26 17:33:45 -0400

    Added Bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog models here.
+
+class Blog(models.Model):
+    #title
+    title = models.CharField(max_length=255)
+    #publication date
+    pub_date = models.DateTimeField()
+    #body/text
+    body = models.TextField()
+    #image
+    image = models.ImageField(upload_to='images/')
+
+#Add the Blog app to the settings.
+
+#Create a migration.
+
+#Migrate.
+
+#Add to the admin
